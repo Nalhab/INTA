1a340ac058ccf199fd6c08a411320e200f4c4859
Mathias Tock <mathias.tock@epita.fr>
2024-10-31 17:55:25 +0100

    First version with keycloak

--- medical-auth-frontend/src/App.js
+++ medical-auth-frontend/src/App.js
@@ -1,22 +1,25 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthProvider from './AuthProvider';
-import PrivateRoute from './PrivateRoute';
-import Home from './Home';
-import Login from './Login';
+import React, { useState, useEffect } from 'react';
+import keycloak from './keycloak';
+import KeycloakProvider from '@react-keycloak/web';
+import Patients from './components/Patients';
 
-function App() {
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required' }).then((auth) => {
+      setAuthenticated(auth);
+    });
+  }, []);
+
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<PrivateRoute component={Home} />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <KeycloakProvider keycloak={keycloak}>
+      <div className="App">
+        <h1>Application Médicale</h1>
+        {authenticated ? <Patients /> : <div>Veuillez vous connecter</div>}
+      </div>
+    </KeycloakProvider>
   );
-}
+};
 
 export default App;
